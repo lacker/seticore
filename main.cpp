f2a1ff369f8f6dbe9845c64aaf1f573d29577727
Kevin Lacker <lacker@gmail.com>
2022-04-29 15:45:57 -0700

    command line arg parsing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <string>
 
@@ -5,16 +6,28 @@
 
 using namespace std;
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cerr << "usage: seticore <h5file>" << endl;
+  po::options_description desc("seticore options");
+  desc.add_options()
+    ("input", po::value<string>(), "the input .h5 file")
+    ;
+
+  po::positional_options_description p;
+  p.add("input", -1);
+  
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  po::notify(vm);
+  
+  if (!vm.count("input")) {
+    cerr << desc << "\n";
     return 1;
   }
 
-  // Open the file
-  string filename = string(argv[1]);
+  string filename = vm["input"].as<string>();
   cout << "argument is: " << filename << endl;
-
   dedoppler(filename);
 }
 
