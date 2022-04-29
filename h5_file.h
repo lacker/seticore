3f588781c3a9e6aea8a690b5214954080b046200
Kevin Lacker <lacker@gmail.com>
2022-04-29 15:24:55 -0700

    make some stuff private

--- h5_file.h
+++ h5_file.h
@@ -1,14 +1,20 @@
 using namespace std;
 
+/*
+  This class contains helper methods for processing .h5 files that are specific to the
+  astronomical data format we expect, like extracting particular data from headers.
+ */
 class H5File {
+ private:
+  void getDoubleAttr(const string &name, double* output);
+  hid_t file, dataset, dataspace;
+
  public:
   H5File(const string& filename);
   ~H5File();
   
-  hid_t file, dataset, dataspace;
   double tsamp, foff;
   int num_timesteps, num_freqs, coarse_channel_size, num_coarse_channels;
   
-  void getDoubleAttr(const string &name, double* output);
   void loadCoarseChannel(int i, float* output);
 };
