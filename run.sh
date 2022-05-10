0aeecff231d25f9431b01b3b3a0f81f2a9c833c6
Kevin Lacker <lacker@gmail.com>
2022-05-10 13:18:09 -0700

    only diff when golden.txt exists

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 H5=data/blc17_guppi_59544_62191_HIP99317_0059.rawspec.0000.h5
 ./build/seticore $H5 --max_drift=0.4 --snr=10 --min_drift=0 | tee data/output.txt
-echo diffing
-diff data/output.txt data/golden.txt
+if [ -f "data/golden.txt" ]; then
+    echo diffing
+    diff data/output.txt data/golden.txt
+fi
