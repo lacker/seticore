2ffe116a121ba87877d8f59060503e05baf43853
Kevin Lacker <lacker@gmail.com>
2022-05-31 22:46:18 -0700

    make some fields private

--- dedoppler.h
+++ dedoppler.h
@@ -24,20 +24,40 @@ class Dedopplerer {
 public:
   // How many timesteps of data are processed by the engine.
   // They may be stored in a larger array.
-  int num_timesteps;
+  const int num_timesteps;
 
   // How many frequency channels of data are processed by the engine.
-  int num_channels;
+  const int num_channels;
+
+  // Frequency difference between adjacent bins, in MHz
+  const double foff;
+
+  // Time difference between adjacent bins, in seconds
+  const double tsamp;
+
+  // Whether the data we receive has a DC spike
+  const bool has_dc_spike;
+  
+  // The input array is on the GPU, used to accept data from the previous step in
+  // the data processing pipeline.
+  float *input;
+
+  Dedopplerer(int num_timesteps, int num_channels, double foff, double tsamp, bool has_dc_spike);
+  ~Dedopplerer();
+
+  void processInput(double max_drift, double min_drift, double snr_threshold,
+                    vector<DedopplerHit>* output);
+  
+private:
+  // For computing Taylor sums, we use three unified memory arrays,
+  // each the size of one coarse channel. The input array is to read the source
+  // data, and these two buffers are to use for Taylor tree calculations.
+  float *buffer1, *buffer2;
 
   // The size of the larger array that contains the number of timesteps,
   // typically rounded up to the nearest power of two.
   int rounded_num_timesteps;
-
-  // For computing Taylor sums, we use three unified memory arrays,
-  // each the size of one coarse channel. One array to read the source
-  // data, and two buffers to use for Taylor tree calculations.
-  float *input, *buffer1, *buffer2;
-
+  
   // For normalization, we sum each column.
   float *column_sums;
 
@@ -48,26 +68,9 @@ public:
   float *top_path_sums;
   int *top_drift_blocks, *top_path_offsets;
 
-  // Frequency difference between adjacent bins, in MHz
-  double foff;
-
-  // Time difference between adjacent bins, in seconds
-  double tsamp;
-  
   // How many timesteps the signal drifts in our data
   int drift_timesteps;
 
   // The difference in adjacent drift rates that we look for, in Hz/s
-  double drift_rate_resolution;
-
-  // Whether the data we receive has a DC spike
-  bool has_dc_spike;
-
-  Dedopplerer(int num_timesteps, int num_channels, double foff, double tsamp, bool has_dc_spike);
-  ~Dedopplerer();
-
-  void processInput(double max_drift, double min_drift, double snr_threshold,
-                    vector<DedopplerHit>* output);
-
-  
+  double drift_rate_resolution;  
 };
