aa097836c3679ee3c1269b24592105e0f8f954d4
Kevin Lacker <lacker@gmail.com>
2022-08-02 14:04:10 -0700

    more info in rawls

--- rawls.cpp
+++ rawls.cpp
@@ -32,6 +32,7 @@ int main(int argc, char* argv[]) {
     cout << fmt::format("{:.1f} MHz center frequency\n", group.obsfreq);
     cout << pluralize(group.num_coarse_channels, "coarse channel") << endl;
     cout << pluralize(group.timesteps_per_block, "timestep") << " per block\n";
+    cout << (group.read_size / 1024 / 1024) << " MB block size\n";
     cout << pluralize(group.num_blocks, "total block") << endl;
     cout << fmt::format("{:.1f} GB total data\n", group.totalDataGB());
     cout << fmt::format("{:.1f}s total time\n", group.totalTime());
