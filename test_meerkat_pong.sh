e5d27d9f089966cc551e359117cc2e1bfcf25834
Kevin Lacker <lacker@gmail.com>
2022-08-04 00:29:43 +0200

    testing on 4k mode dataset

--- test_meerkat_pong.sh
+++ test_meerkat_pong.sh
@@ -10,13 +10,20 @@
 # If not, run:
 #   sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
 
-# Testing the smaller May 12 dataset on blpn129
-# INPUT_DIR="/home/lacker/benchmark"
-# RECIPE_DIR="/home/davidm/bfr5.test/"
+# The fft sizes for different modes
+FFT1K=524288
+FFT4K=131072
+FFT32K=16384
 
 # Testing the July 19 dataset on blpn130
-INPUT_DIR="/buf0ro/20220719/0026/Unknown/GUPPI/"
+# INPUT_DIR="/buf0ro/20220719/0026/Unknown/GUPPI/"
+# RECIPE_DIR="/home/obs/bfr5/"
+# FFT_SIZE=$FFT1K
+
+# Testing the August 1 dataset on blpn131
+INPUT_DIR="/buf0ro/20220801/0055/Unknown/GUPPI/"
 RECIPE_DIR="/home/obs/bfr5/"
+FFT_SIZE=$FFT4K
 
 OUTPUT_DIR="/mydatag/test/lacker/"
 
@@ -33,6 +40,6 @@ nsys profile -t nvtx,cuda \
     --snr 10.0 \
     --recipe_dir $RECIPE_DIR \
     --num_bands 16 \
-    --fft_size 131072 \
+    --fft_size $FFT_SIZE \
     --telescope_id 64
     
