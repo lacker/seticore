5f33a424638bf0b1521b9a3fb1f350ab9fd47858
Kevin Lacker <lacker@gmail.com>
2022-09-06 18:58:24 +0200

    update extraction test for meerkat data

--- test_extract.sh
+++ test_extract.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -e
 
-# Try to extract this hit:
-#
-# found 2 hits in beam 42, coarse channel 0
-#   index = 86216, drift steps = 1, snr = 12.3893, drift rate = 0.317774
-#   <snipped>
+# Extracting a hit from blpn129
+
 
 ./build/extract \
-    --raw_prefix=../data/beamtest/guppi_59712_16307_003760_J1939-6342_0001 \
-    --output=data/test.stamp \
-    --band=0 \
-    --num_bands=32 \
+    --raw_prefix=/buf0ro/20220816/guppi_59806_84779_003380_J1939-6342_0001 \
+    --output=/mydatag/test/lacker/20220816.stamp \
+    --band=12 \
+    --num_bands=16 \
     --coarse_channel=0 \
     --fft_size=131072 \
-    --start_channel=86100 \
-    --num_channels=300 \
+    --start_channel=38417 \
+    --num_channels=80 \
     --telescope_id=64
 
 echo done
