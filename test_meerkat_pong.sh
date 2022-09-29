167f86f1245143726bbf31ad6333cb31cc0b24ce
Kevin Lacker <lacker@gmail.com>
2022-09-29 18:30:05 +0200

    reproduce questionable run of sep 28

--- test_meerkat_pong.sh
+++ test_meerkat_pong.sh
@@ -15,12 +15,16 @@ FFT1K=524288
 FFT4K=131072
 FFT32K=16384
 
-# Testing the July 19 dataset on blpn130
-if false; then
-    INPUT_DIR="/buf0ro/20220719/0026/Unknown/GUPPI/"
+OUTPUT_DIR="/mydatag/test/lacker/"
+
+# Testing the Sep 28 dataset on blpn130
+if true; then
+    INPUT_DIR="/buf0ro/lacker/small"
     RECIPE_DIR="/home/obs/bfr5/"
-    FFT_SIZE=$FFT1K
+    FFT_SIZE=$FFT4K
     NUM_BANDS=16
+    OUTPUT_DIR="/scratch/data/lacker/seticore/"
+    EXTRA_FLAGS="--h5_dir $OUTPUT_DIR"
 fi
 
 # Testing the August 1 dataset on blpn131
@@ -32,14 +36,13 @@ if false; then
 fi
 
 # Testing the August 16 dataset on blpn129
-if true; then
+if false; then
     INPUT_DIR="/buf0/20220816/"
     RECIPE_DIR="/home/obs/bfr5/"
     FFT_SIZE=$FFT4K
     NUM_BANDS=16
 fi
     
-OUTPUT_DIR="/mydatag/test/lacker/"
 
 
 # Remove old reports
@@ -53,9 +56,10 @@ rm -f *qdrep
     --output $OUTPUT_DIR \
     --max_drift 10.0 \
     --min_drift 0.0 \
-    --snr 6.0 \
+    --snr 10.0 \
     --recipe_dir $RECIPE_DIR \
     --num_bands $NUM_BANDS \
     --fft_size $FFT_SIZE \
-    --telescope_id 64
+    --telescope_id 64 \
+    $EXTRA_FLAGS
     
