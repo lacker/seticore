678c5713f1d9e881a2b5ab53966bf8dffbc03de5
Kevin Lacker <lacker@gmail.com>
2022-09-29 14:17:09 -0700

    blockls tool

---
+++ blockls.cpp
@@ -0,0 +1,27 @@
+#include <fmt/core.h>
+#include <iostream>
+
+#include "raw_file.h"
+
+using namespace std;
+
+/*
+  Displays block-by-block information about a raw file.
+
+  Usage:
+    blockls <filename>
+ */
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cerr << "usage: blockls <filename>\n";
+    exit(1);
+  }
+  string fname(argv[1]);
+
+  RawFile f(fname);
+  for (auto& header : f.headers()) {
+    vector<char> buffer(header.blocsize);
+    f.reader().readBand(header, 0, 1, &buffer[0]);
+    cout << "pktidx " << header.pktidx << ", data[0] = " << (int)(buffer[0]) << endl;
+  }
+}
