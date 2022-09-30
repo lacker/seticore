e0f7b5fd8a5466cbed43274e0d5878fb54ec31c9
Kevin Lacker <lacker@gmail.com>
2022-09-29 22:17:20 -0700

    expand output of blockls

--- blockls.cpp
+++ blockls.cpp
@@ -22,6 +22,9 @@ int main(int argc, char* argv[]) {
   for (auto& header : f.headers()) {
     vector<char> buffer(header.blocsize);
     assert(f.reader().readBand(header, 0, 1, &buffer[0]));
-    cout << "pktidx " << header.pktidx << ", data[0] = " << (int)(buffer[0]) << endl;
+    cout << "pktidx " << header.pktidx << ", data[:3] = "
+         << (int)(buffer[0]) << ", "
+         << (int)(buffer[1]) << ", "
+         << (int)(buffer[2]) << endl;
   }
 }
