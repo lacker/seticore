829f32cbf0c2159e1bdfc6c746eeaa2a3e41174c
Kevin Lacker <lacker@gmail.com>
2022-10-15 14:07:59 -0700

    0.2.16 fixes load dependency issue

--- util.h
+++ util.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const string VERSION = "0.2.15";
+const string VERSION = "0.2.16";
 
 // This is allegedly a SIGPROC standard but the most authoritative source
 // I can find is:
