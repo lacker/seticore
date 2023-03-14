f5eb021231324f0a8d1451a36f1fea0fe490060e
Kevin Lacker <lacker@gmail.com>
2023-03-14 15:42:49 -0700

    make the downloading script get a whole cadence of voyager data

--- download_test_data.sh
+++ download_test_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-H5=blc17_guppi_59544_62191_HIP99317_0059.rawspec.0000.h5
-DATA_URL="https://bldata.berkeley.edu/pipeline/AGBT21B_999_31/blc17_blp17/$H5"
+for URL in `cat ./test_data_files.txt`; do
+    H5=`basename $URL`
+    if [ ! -f "data/$H5" ]; then
+        wget $URL -P data/
+    fi
+done
 
-if [ ! -f "data/$H5" ]; then
-    echo downloading h5 data for regression testing...
-    wget $DATA_URL -P data/
-fi
